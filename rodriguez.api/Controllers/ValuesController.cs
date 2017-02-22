f8121643c38be118d871471f26fc4e37d7a42ba5
Josue Martinez <jmh_3@hotmail.com>
2017-02-22 16:03:40 -0400

    controller para categorias

--- rodriguez.api/Controllers/ValuesController.cs
+++ rodriguez.api/Controllers/ValuesController.cs
@@ -1,24 +1,29 @@
-﻿using System;
+﻿using rodriguez.api.Clases;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace rodriguez.api.Controllers
 {
+    [EnableCors(origins:"*", headers:"*", methods:"*")]
     public class ValuesController : ApiController
     {
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "VALUE", "value2" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return Utilidades.capitalize("value");
         }
 
         // POST api/values
---
+++ rodriguez.api/Controllers/categoriasController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using rodriguez.api.Models;
+using rodriguez.api.Clases;
+
+namespace rodriguez.api.Controllers
+{
+    public class categoriasController : ApiController
+    {
+        private RodriguezModel db = new RodriguezModel();
+
+        //Obtener todas las categorias
+        // GET: api/categorias
+        public IQueryable<categoria> Getcategoria()
+        {
+            return db.categoria;
+        }
+
+        //Obtener una categoria en especifico
+        // GET: api/categorias/{id}
+        [ResponseType(typeof(categoria))]
+        public async Task<IHttpActionResult> Getcategoria(int id)
+        {
+            categoria categoria = await db.categoria.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+
+        //Actualizar categoria
+        // PUT: api/categorias/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putcategoria(int id, categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != categoria.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!categoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        //Insertar categoria
+        // POST: api/categorias
+        [ResponseType(typeof(categoria))]
+        public async Task<IHttpActionResult> Postcategoria(categoria categoria)
+        {
+            if (categoria == null)  //el request no contiene categoria
+            {
+                return BadRequest(ModelState);
+            }
+
+            //si la descripcion de la categoria esta en blanco
+            if(String.IsNullOrEmpty(categoria.descripcion) || String.IsNullOrWhiteSpace(categoria.descripcion))
+            {
+                return BadRequest("La categoría debe tener una descripción.");
+            }
+
+            try
+            {
+                categoria.descripcion = Utilidades.capitalize(categoria.descripcion);
+                //verificar si no existe una categoria con el mismo nombre
+                if(db.categoria.Where(x => x.descripcion.Equals(categoria.descripcion)).Count() == 0)
+                {
+                    db.categoria.Add(categoria);
+                    await db.SaveChangesAsync();
+                    return CreatedAtRoute("DefaultApi", new { id = categoria.id }, categoria);
+                }
+                return BadRequest("Esta categoría ya existe");
+            }
+            catch(Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        // DELETE: api/categorias/5
+        [ResponseType(typeof(categoria))]
+        public async Task<IHttpActionResult> Deletecategoria(int id)
+        {
+            categoria categoria = await db.categoria.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            db.categoria.Remove(categoria);
+            await db.SaveChangesAsync();
+
+            return Ok(categoria);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool categoriaExists(int id)
+        {
+            return db.categoria.Count(e => e.id == id) > 0;
+        }
+    }
+}
