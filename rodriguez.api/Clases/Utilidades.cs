9824e4b4d160665bdc14ffabb194a5cf4ed7d10e
Josue Martinez <jmh_3@hotmail.com>
2017-02-22 16:03:14 -0400

    creando clase para utilidades variadas

---
+++ rodriguez.api/Clases/Utilidades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace rodriguez.api.Clases
+{
+    public static class Utilidades
+    {
+        public static String capitalize(String text)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            text = textInfo.ToLower(text);  //evitando que no se consideren acronimos
+            return textInfo.ToTitleCase(text);
+        }
+    }
+}
