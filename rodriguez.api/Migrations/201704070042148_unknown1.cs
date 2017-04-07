3c539e5976a61bcd964d98caa17b5622473105a3
Josue Martinez <jmh_3@hotmail.com>
2017-04-06 21:08:08 -0400

    deploying migrations

---
+++ rodriguez.api/Migrations/201704070042148_unknown1.cs
@@ -0,0 +1,18 @@
+namespace rodriguez.api.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class unknown1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("tasamoneda", "activo");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("tasamoneda", "activo", c => c.Boolean(nullable: false));
+        }
+    }
+}
