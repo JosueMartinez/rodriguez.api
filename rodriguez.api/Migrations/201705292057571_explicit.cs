24180ec483d14d9febfd00201514b268bcec7075
Josue Martinez <jmh_3@hotmail.com>
2017-05-29 17:00:41 -0400

    explicit migration

---
+++ rodriguez.api/Migrations/201705292057571_explicit.cs
@@ -0,0 +1,16 @@
+namespace rodriguez.api.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _explicit : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
