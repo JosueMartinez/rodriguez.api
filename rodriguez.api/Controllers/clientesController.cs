05ed0b047b5717df80a125b2398a2f1a2566ab01
Josue Martinez <jmh_3@hotmail.com>
2017-08-08 16:34:01 -0400

    controller for clients and usuarios

---
+++ rodriguez.api/Controllers/clientesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using rodriguez.api.Models;
+
+namespace rodriguez.api.Controllers
+{
+    public class clientesController : ApiController
+    {
+        private RodriguezModel db = new RodriguezModel();
+
+        // GET: api/clientes
+        public IQueryable<cliente> Getclientes()
+        {
+            return db.clientes;
+        }
+
+        // GET: api/clientes/5
+        [ResponseType(typeof(cliente))]
+        public async Task<IHttpActionResult> Getcliente(int id)
+        {
+            cliente cliente = await db.clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // GET: api/clientes/5
+        [ResponseType(typeof(bono))]
+        [Route("api/clienteU/{usuario}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetclienteNombre(string usuario)
+        {
+            cliente cliente = await db.clientes.Where(x => x.usuario.Equals(usuario)).FirstOrDefaultAsync();
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/clientes/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putcliente(int id, cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cliente.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!clienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/clientes
+        [ResponseType(typeof(cliente))]
+        public async Task<IHttpActionResult> Postcliente(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.clientes.Add(cliente);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = cliente.id }, cliente);
+        }
+
+        // DELETE: api/clientes/5
+        [ResponseType(typeof(cliente))]
+        public async Task<IHttpActionResult> Deletecliente(int id)
+        {
+            cliente cliente = await db.clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.clientes.Remove(cliente);
+            await db.SaveChangesAsync();
+
+            return Ok(cliente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool clienteExists(int id)
+        {
+            return db.clientes.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ rodriguez.api/Controllers/usuariosController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using rodriguez.api.Models;
+
+namespace rodriguez.api.Controllers
+{
+    public class usuariosController : ApiController
+    {
+        private RodriguezModel db = new RodriguezModel();
+
+        // GET: api/usuarios
+        public IQueryable<usuario> Getusuarios()
+        {
+            return db.usuarios;
+        }
+
+        // GET: api/usuarios/5
+        [ResponseType(typeof(usuario))]
+        public async Task<IHttpActionResult> Getusuario(int id)
+        {
+            usuario usuario = await db.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // GET: api/clientes/5
+        [ResponseType(typeof(bono))]
+        [Route("api/usuarioU/{usuario}")]
+        [HttpGet] //
+        public async Task<IHttpActionResult> GetclienteNombre(string usuario)
+        {
+            usuario u = await db.usuarios.Where(x => x.nombreUsuario.ToLower().Equals(usuario.ToLower())).FirstOrDefaultAsync();
+            if (u == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(u);
+        }
+
+        // PUT: api/usuarios/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putusuario(int id, usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!usuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/usuarios
+        [ResponseType(typeof(usuario))]
+        public async Task<IHttpActionResult> Postusuario(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.usuarios.Add(usuario);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.id }, usuario);
+        }
+
+        // DELETE: api/usuarios/5
+        [ResponseType(typeof(usuario))]
+        public async Task<IHttpActionResult> Deleteusuario(int id)
+        {
+            usuario usuario = await db.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.usuarios.Remove(usuario);
+            await db.SaveChangesAsync();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool usuarioExists(int id)
+        {
+            return db.usuarios.Count(e => e.id == id) > 0;
+        }
+    }
+}
