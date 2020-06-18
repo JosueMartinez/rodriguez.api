58d8a7c06a358279de89880287db3af518b29a56
JosueMartinez <josue.martinezzh@gmail.com>
2020-06-18 13:16:00 -0400

    exception logging to file

---
+++ rodriguez.api/Clases/AutoMapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Rodriguez.Data.DTOs;
+using Rodriguez.Data.Models;
+
+namespace rodriguez.api.Clases
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration() : this("MapperConfg") { }
+
+        protected AutoMapperConfiguration(string profileName) : base(profileName)
+        {
+            CreateMap<TasaMoneda, TasaDto>()
+                .ForMember(dest => dest.Simbolo, opt => opt.MapFrom(src => src.Moneda.Simbolo))
+                .ForMember(dest => dest.Moneda, opt => opt.MapFrom(src => src.Moneda.Descripcion));
+        }
+
+    }
+}
